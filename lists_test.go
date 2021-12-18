4bb42d114b960e18f86198944dd416ea89716248
Hernando Gisinger <hgisinger@gmail.com>
2021-12-18 12:38:03 -0300

    Update tests

--- lists_test.go
+++ lists_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestMap(t *testing.T) {
-	doubles := Map([]int{1, 2, 3}, func(x int) int { return x * 2 })
+	doubles := Map(func(x int) int { return x * 2 }, []int{1, 2, 3})
 	if doubles[0] != 2 || doubles[1] != 4 || doubles[2] != 6 {
-		t.Error("Map([]int{1, 2, 3}, func(x int) int { return x * 2 }) != []int{2, 4, 6}")
+		t.Error("Map(func(x int) int { return x * 2 }, []int{1, 2, 3}) != []int{2, 4, 6}")
 	}
-	toString := Map([]int{1, 2, 3}, strconv.Itoa)
+	toString := Map(strconv.Itoa, []int{1, 2, 3})
 	if toString[0] != "1" || toString[1] != "2" || toString[2] != "3" {
-		t.Error(`Map([]int{1, 2, 3}, strconv.Itoa) != []string{"1", "2", "3"}`)
+		t.Error(`Map(strconv.Itoa, []int{1, 2, 3}) != []string{"1", "2", "3"}`)
 	}
 }
